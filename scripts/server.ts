1ecd66f2692292d0a15e26d463d7ec6e953205b3
Achintya Kumar <32974311+achintyaakumar@users.noreply.github.com>
2020-04-24 18:16:10 -0400

    part 1

---
+++ scripts/server.ts
@@ -0,0 +1,33 @@
+const faker = require('faker');
+const fs = require('fs')
+const express = require('express');
+const app = express();
+
+function generateUsers() {
+
+  let users = []
+
+  for (let id=1; id <= 2; id++) {
+    let firstName = faker.name.firstName();
+    let lastName = faker.name.lastName();
+    let username = firstName+lastName+id;
+    let email = faker.internet.email();
+
+    users.push({
+        "username": username,
+        "first_name": firstName,
+        "last_name": lastName,
+        "email": email
+    });
+  }
+
+  return { "data": users }
+}
+
+let dataObj = generateUsers();
+
+fs.writeFileSync('data.json', JSON.stringify(dataObj, null, '\t'));
+
+app.get("/home", (req, res) => {
+    res.sendFile(__dirname + '/data.json');
+ });

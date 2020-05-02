e22c0d4d2e1b73db2f4d4734a27f2043a21ab8ca
Manan Patel <mjpatel@umass.edu>
2020-05-01 23:42:32 -0400

    removed server files

--- scripts/main_server.ts
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-import { Database } from './mongodb'
-import { MyServer } from './server';
-
-const theDatabase = new Database('manan');
-const theServer = new MyServer(theDatabase);
-
-theServer.listen(process.env.PORT || 8080);

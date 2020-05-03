f0381c125e8d60d5612d89eda2d3eedde289d7ef
Manan Patel <mjpatel@umass.edu>
2020-05-03 02:56:47 -0400

    updating changes in server.js file

--- scripts/server.js
+++ scripts/server.js
@@ -73,29 +73,21 @@ var Server = /** @class */ (function () {
             });
         }); });
     }
-    Server.prototype.registerHandler = function (request, response) {
+
+    //  CRUD handlers
+
+
+    Server.prototype.createWatchListHandler = function (request, response) {
         return __awaiter(this, void 0, void 0, function () {
-            return __generator(this, function (_a) {
-                response.write(JSON.stringify({ success: true }));
-                response.end();
-                return [2 /*return*/];
-            });
-        });
-    };
-    Server.prototype.loginHandler = function (request, response) {
-        return __awaiter(this, void 0, void 0, function () {
-            return __generator(this, function (_a) {
-                response.write(JSON.stringify({ jwt: "JSON Web Token", success: true }));
-                response.end();
-                return [2 /*return*/];
-            });
-        });
-    };
-    Server.prototype.resultHandler = function (request, response) {
-        return __awaiter(this, void 0, void 0, function () {
+            var username, WatchListObj;
             return __generator(this, function (_a) {
                 switch (_a.label) {
-                    case 0: return [4 /*yield*/, fakeData_1["default"].generateMedia(request.body, response)];
+                    case 0:
+                        username = request.params['username'];
+                        console.log('username: ' + username);
+                        WatchListObj = request.body.WatchListObj;
+                        console.log(WatchListObj);
+                        return [4 /*yield*/, this.createWatchList(username, WatchListObj, response)];
                     case 1:
                         _a.sent();
                         return [2 /*return*/];
@@ -103,18 +95,216 @@ var Server = /** @class */ (function () {
             });
         });
     };
-    Server.prototype.watchlistHandler = function (request, response) {
+    Server.prototype.readWatchListHandler = function (request, response) {
         return __awaiter(this, void 0, void 0, function () {
+            var username, category, label;
             return __generator(this, function (_a) {
                 switch (_a.label) {
-                    case 0: return [4, fakeData_1["default"].generateWatchlist(request.body, response)];
+                    case 0:
+                        username = request.params['username'];
+                        category = request.query.category;
+                        label = request.query.label;
+                        console.log('------ username, category, label: ' + username + ", " + category + " , " + label);
+                        return [4 /*yield*/, this.readWatchList(username, category, label, response)];
                     case 1:
                         _a.sent();
-                        return [2];
+                        return [2 /*return*/];
                 }
             });
         });
     };
+    Server.prototype.readAllWatchListHandler = function (request, response) {
+        return __awaiter(this, void 0, void 0, function () {
+            var username;
+            return __generator(this, function (_a) {
+                switch (_a.label) {
+                    case 0:
+                        username = request.params['username'];
+                        return [4 /*yield*/, this.readAllWatchLists(username, response)];
+                    case 1:
+                        _a.sent();
+                        return [2 /*return*/];
+                }
+            });
+        });
+    };
+    Server.prototype.updateWatchListHandler = function (request, response) {
+        return __awaiter(this, void 0, void 0, function () {
+            var username, WatchListObj;
+            return __generator(this, function (_a) {
+                switch (_a.label) {
+                    case 0:
+                        username = request.params['username'];
+                        WatchListObj = request.body.WatchListObj;
+                        return [4 /*yield*/, this.updateWatchList(username, WatchListObj, response)];
+                    case 1:
+                        _a.sent();
+                        return [2 /*return*/];
+                }
+            });
+        });
+    };
+    Server.prototype.deleteWatchListHandler = function (request, response) {
+        return __awaiter(this, void 0, void 0, function () {
+            var WatchListObj, username;
+            return __generator(this, function (_a) {
+                switch (_a.label) {
+                    case 0:
+                        console.log("in deleteWatchListHandler");
+                        // WatchListObj = {//make changes here
+                       
+
+                        //         }]//make changes here 
+                        // };
+                        username = request.param['username'];
+                        console.log('------ username, WatchList: ' + username + ', ' + WatchListObj);
+                        return [4 /*yield*/, this.deleteWatchList(username, WatchListObj, response)];
+                    case 1:
+                        _a.sent();
+                        return [2 /*return*/];
+                }
+            });
+        });
+    };
+
+    // CRUD functions 
+    Server.prototype.createWatchList = function (username, WatchListObj, response) {
+        return __awaiter(this, void 0, void 0, function () {
+            return __generator(this, function (_a) {
+                switch (_a.label) {
+                    case 0:
+                        console.log("creating WatchList...");
+                        return [4 /*yield*/, this.theDatabase.put(username, WatchListObj)];
+                    case 1:
+                        _a.sent();
+                        response.write(JSON.stringify({ 'result': 'created',
+                            'username': username,
+                            'WatchListObj': WatchListObj
+                        }));
+                        response.end();
+                        return [2 /*return*/];
+                }
+            });
+        });
+    };
+    Server.prototype.readWatchList = function (username, category, label, response) {
+        return __awaiter(this, void 0, void 0, function () {
+            return __generator(this, function (_a) {
+                console.log("reading WatchList...");
+                //await this.theDatabase.get(username, category, label);
+                response.write(JSON.stringify({ 'result': 'read',
+                    'username': username,
+                    'category': category,
+                    'label': label,
+                    'title': faker.random.word() + " WatchList",
+                    'notes': faker.random.words() + " WatchList",
+                    'bookmarks': faker.date.recent() + " - " + faker.random.words() + ", " + faker.date.recent() + " - " + faker.random.words()
+                }));
+                response.end();
+                return [2 /*return*/];
+            });
+        });
+    };
+    Server.prototype.readAllWatchLists = function (username, response) {
+        return __awaiter(this, void 0, void 0, function () {
+            var userObj, labelWatchLists;
+            return __generator(this, function (_a) {
+                switch (_a.label) {
+                    case 0:
+                        console.log("\n\nreading all WatchLists...");
+                        return [4 /*yield*/, this.theDatabase.getAll(username)];
+                    case 1:
+                        userObj = _a.sent();
+                        labelWatchLists = this.parseLabelWatchLists(userObj);
+                        response.write(JSON.stringify({ 'result': 'read all WatchLists',
+                            'username': username,
+                            'WatchListData': labelWatchLists
+                        }));
+                        response.end();
+                        return [2 /*return*/];
+                }
+            });
+        });
+    };
+    Server.prototype.parseLabelWatchLists = function (WatchListObj) {
+        console.log("parsing usr obj");
+        var labelWatchLists = WatchListObj.categories[0].labels;
+        return labelWatchLists;
+    };
+    Server.prototype.updateWatchList = function (username, WatchListObj, response) {
+        return __awaiter(this, void 0, void 0, function () {
+            return __generator(this, function (_a) {
+                console.log("updating WatchList...");
+                //await this.theDatabase.put(username, WatchListObj);
+                response.write(JSON.stringify({
+                    'result': 'updated',
+                    'username': username,
+                    'updatedWatchListData': WatchListObj
+                }));
+                response.end();
+                return [2 /*return*/];
+            });
+        });
+    };
+    Server.prototype.deleteWatchList = function (username, WatchListObj, response) {
+        return __awaiter(this, void 0, void 0, function () {
+            return __generator(this, function (_a) {
+                console.log("deleting WatchList");
+                // await this.theDatabase.del(WatchListObj);
+                response.write(JSON.stringify({ 'result': 'deleted',
+                    'username': username,
+                    'WatchList': WatchListObj
+                }));
+                response.end();
+                return [2 /*return*/];
+            });
+        });
+    };
     return Server;
+
+
+    // Server.prototype.registerHandler = function (request, response) {
+    //     return __awaiter(this, void 0, void 0, function () {
+    //         return __generator(this, function (_a) {
+    //             response.write(JSON.stringify({ success: true }));
+    //             response.end();
+    //             return [2 /*return*/];
+    //         });
+    //     });
+    // };
+    // Server.prototype.loginHandler = function (request, response) {
+    //     return __awaiter(this, void 0, void 0, function () {
+    //         return __generator(this, function (_a) {
+    //             response.write(JSON.stringify({ jwt: "JSON Web Token", success: true }));
+    //             response.end();
+    //             return [2 /*return*/];
+    //         });
+    //     });
+    // };
+    // Server.prototype.resultHandler = function (request, response) {
+    //     return __awaiter(this, void 0, void 0, function () {
+    //         return __generator(this, function (_a) {
+    //             switch (_a.label) {
+    //                 case 0: return [4 /*yield*/, fakeData_1["default"].generateMedia(request.body, response)];
+    //                 case 1:
+    //                     _a.sent();
+    //                     return [2 /*return*/];
+    //             }
+    //         });
+    //     });
+    // };
+    // Server.prototype.watchlistHandler = function (request, response) {
+    //     return __awaiter(this, void 0, void 0, function () {
+    //         return __generator(this, function (_a) {
+    //             switch (_a.label) {
+    //                 case 0: return [4, fakeData_1["default"].generateWatchlist(request.body, response)];
+    //                 case 1:
+    //                     _a.sent();
+    //                     return [2];
+    //             }
+    //         });
+    //     });
+    // };
+    //return Server;
 }());
 exports.Server = Server;
